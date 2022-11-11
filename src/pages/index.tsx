c572e5bc758cfc43ab26944c13a2366698823064
Rafael <1rafaelrocha2@gmail.com>
2022-11-11 16:49:51 -0300

    feat: create new project

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Create from "../components/Create";
+import Read from "../components/Read";
+
+export default function Home() {
+  return (
+    <>
+      <Create />
+      <Read />
+    </>
+  )
+}
